3276ecce72b00c58f55a18918951c5d7359623a6
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-03-08 22:54:07 +0000

    Deploying to gh-pages from @ LTLA/chihaya@77980f762eb42f7183886837d028909d075fbaf1 🚀

--- docs/search/functions_0.js
+++ docs/search/functions_0.js
@@ -4,5 +4,6 @@ var searchData=
   ['validate_5fcombine_1',['validate_combine',['../combine_8hpp.html#a8f0fee592338598a7ae6e691b1098a3a',1,'chihaya']]],
   ['validate_5fexternal_2',['validate_external',['../external_8hpp.html#a475328cf510920ba9e8762408f3fc100',1,'chihaya']]],
   ['validate_5fsubset_3',['validate_subset',['../subset_8hpp.html#a0d5d52b5f86f113d2c029157d8b5b8c9',1,'chihaya']]],
-  ['validate_5ftranspose_4',['validate_transpose',['../transpose_8hpp.html#ac978cbb9677f3c175859f074fa48cd5a',1,'chihaya']]]
+  ['validate_5fsubset_5fassignment_4',['validate_subset_assignment',['../subset__assignment_8hpp.html#ae3e247db7e908418fdd164dcc7904e67',1,'chihaya']]],
+  ['validate_5ftranspose_5',['validate_transpose',['../transpose_8hpp.html#ac978cbb9677f3c175859f074fa48cd5a',1,'chihaya']]]
 ];
