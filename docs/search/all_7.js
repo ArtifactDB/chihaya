64d2dd694ed5a3f8079cbe269d1fbd0a64d41250
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-03-10 21:51:12 +0000

    Deploying to gh-pages from @ LTLA/chihaya@3c533e90b5f7c42370cd709f51d7bd7b767b40cd 🚀

--- docs/search/all_7.js
+++ docs/search/all_7.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['subset_2ehpp_0',['subset.hpp',['../subset_8hpp.html',1,'']]],
-  ['subset_5fassignment_2ehpp_1',['subset_assignment.hpp',['../subset__assignment_8hpp.html',1,'']]]
+  ['sparse_5fmatrix_2ehpp_0',['sparse_matrix.hpp',['../sparse__matrix_8hpp.html',1,'']]],
+  ['subset_2ehpp_1',['subset.hpp',['../subset_8hpp.html',1,'']]],
+  ['subset_5fassignment_2ehpp_2',['subset_assignment.hpp',['../subset__assignment_8hpp.html',1,'']]]
 ];
