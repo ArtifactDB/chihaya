f03019dfa433db992faf54f4d901bc478679d128
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-01-16 16:09:19 +0000

    Deploying to gh-pages from @ ArtifactDB/chihaya@00dccdfbde93965f02a4b8c8d79a4aada3c70860 🚀

--- docs/search/all_3.js
+++ docs/search/all_3.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['delayed_20operations_20in_20hdf5_0',['Delayed operations in HDF5',['../index.html',1,'']]],
   ['dense_5farray_2ehpp_1',['dense_array.hpp',['../dense__array_8hpp.html',1,'']]],
-  ['dimensions_2',['dimensions',['../structchihaya_1_1ArrayDetails.html#ad572a229d1da9b90ee688f9b0572b536',1,'chihaya::ArrayDetails']]],
-  ['dimnames_2ehpp_3',['dimnames.hpp',['../dimnames_8hpp.html',1,'']]]
+  ['details_5fonly_2',['details_only',['../structchihaya_1_1Options.html#a675ba55ac4e1acfe61d4ab39a6829673',1,'chihaya::Options']]],
+  ['dimensions_3',['dimensions',['../structchihaya_1_1ArrayDetails.html#ad572a229d1da9b90ee688f9b0572b536',1,'chihaya::ArrayDetails']]],
+  ['dimnames_2ehpp_4',['dimnames.hpp',['../dimnames_8hpp.html',1,'']]]
 ];
--- docs/search/variables_1.js
+++ docs/search/variables_1.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['dimensions_0',['dimensions',['../structchihaya_1_1ArrayDetails.html#ad572a229d1da9b90ee688f9b0572b536',1,'chihaya::ArrayDetails']]]
+  ['details_5fonly_0',['details_only',['../structchihaya_1_1Options.html#a675ba55ac4e1acfe61d4ab39a6829673',1,'chihaya::Options']]],
+  ['dimensions_1',['dimensions',['../structchihaya_1_1ArrayDetails.html#ad572a229d1da9b90ee688f9b0572b536',1,'chihaya::ArrayDetails']]]
 ];
