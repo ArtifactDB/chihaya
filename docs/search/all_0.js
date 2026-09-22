46ece00669ea76787b923350d51fae6f97337012
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2026-09-22 04:05:48 +0000

    Deploying to gh-pages from @ ArtifactDB/chihaya@e7dfa25e11390800c945a8324f44d29ea54cf922 🚀

--- docs/search/all_0.js
+++ docs/search/all_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['array_5fvalidate_5fregistry_0',['array_validate_registry',['../structchihaya_1_1Options.html#a6f85a4bdb3dd99d50a2ee2b6da2a5c43',1,'chihaya::Options']]],
+  ['array_5fvalidate_5fregistry_0',['array_validate_registry',['../structchihaya_1_1Options.html#a387149191b500eee4d3b231f3b0ca725',1,'chihaya::Options']]],
   ['arraydetails_1',['ArrayDetails',['../structchihaya_1_1ArrayDetails.html',1,'chihaya']]],
   ['arraytype_2',['ArrayType',['../namespacechihaya.html#aff4525846a37a56401501e2ffc5a61f2',1,'chihaya']]]
 ];
--- docs/search/variables_0.js
+++ docs/search/variables_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['array_5fvalidate_5fregistry_0',['array_validate_registry',['../structchihaya_1_1Options.html#a6f85a4bdb3dd99d50a2ee2b6da2a5c43',1,'chihaya::Options']]]
+  ['array_5fvalidate_5fregistry_0',['array_validate_registry',['../structchihaya_1_1Options.html#a387149191b500eee4d3b231f3b0ca725',1,'chihaya::Options']]]
 ];
